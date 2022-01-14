c60dec89fc4a9b57bd1941dfd73aff7cf4fee3a5
Jeff Terrell <jeffaterrell@gmail.com>
2022-01-14 15:15:06 -0500

    add Treats controller and corresponding index and create views

---
+++ PierreTreats/Controllers/TreatsController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using PierreTreats.Models;
+
+namespace PierreTreats.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly PierreTreatsContext _db;
+
+    public TreatsController(PierreTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Treats.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
