682ac281a9003982290aa0a4167898c8b299e2fd
Jeff Terrell <jeffaterrell@gmail.com>
2022-01-14 14:27:44 -0500

    add Flavors controller and corresponding index and create views

---
+++ PierreTreats/Controllers/FlavorsController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using PierreTreats.Models;
+
+namespace PierreTreats.Controllers
+{
+  public class FlavorsController : Controller
+  {
+    private readonly PierreTreatsContext _db;
+
+    public FlavorsController(PierreTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Flavors.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
